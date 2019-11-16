484b44df22679a6fd946c06ee1dad21117c890b9
zcw <2455142423@qq.com>
2019-11-16 23:40:06 +0800

    [MOD]readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # xshop
-xshop
+### 简介
+
+电商项目
+
+
+
+### 系统架构
+
+
+
+
+
+### 技术栈
+
+- spring
+- spring mvc
+- mybatis
+- dubbo
+- zookeeper
+- mycat
+- mysql
+- redis
+
+
+
+### 主要功能
+
+
+
+
+
+### 核心技术
+
--- xshop_web_manager/src/main/java/com/xshop/controller/goods/CategoryController.java
+++ xshop_web_manager/src/main/java/com/xshop/controller/goods/CategoryController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author zcw
+ */
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
