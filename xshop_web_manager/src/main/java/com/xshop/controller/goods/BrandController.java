a7f428914ab815bc45cb1ff5f870a3cd155e959e
zcw <2455142423@qq.com>
2019-11-23 01:11:20 +0800

    [ADD]swagger接口可视化

--- xshop_web_manager/src/main/java/com/xshop/controller/goods/BrandController.java
+++ xshop_web_manager/src/main/java/com/xshop/controller/goods/BrandController.java
@@ -5,6 +5,8 @@ import com.xshop.entity.CommonResponse;
 import com.xshop.entity.PageResult;
 import com.xshop.pojo.goods.Brand;
 import com.xshop.service.goods.BrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,71 +17,57 @@ import java.util.Map;
  * @author zcw
  * @date 2019/11/9
  */
+@Api(tags = "品牌控制器类")
 @RestController
 @RequestMapping("/brand")
 public class BrandController {
     @Reference
     private BrandService brandService;
 
-    /**
-     * 查询所有
-     */
+
+    @ApiOperation("查询所有品牌")
     @GetMapping("/findAll.do")
     public List<Brand> findAll() {
         return brandService.findAll();
     }
 
-    /**
-     * 分页查询
-     */
+    @ApiOperation("分页查询")
     @GetMapping("/findPage.do")
     public PageResult<Brand> findPage(int page, int size) {
         return brandService.findPage(page, size);
     }
 
-    /**
-     * 条件查询
-     */
+    @ApiOperation("条件查询")
     @PostMapping("/findList.do")
     public List<Brand> findList(@RequestBody Map<String, Object> searchMap) {
         return brandService.findList(searchMap);
     }
 
-    /**
-     * 条件查询并分页
-     */
+    @ApiOperation("条件查询并分页")
     @PostMapping("/findPage.do")
     public PageResult<Brand> findPage(@RequestBody Map<String, Object> searchMap, int page, int size) {
         return brandService.findPage(searchMap, page, size);
     }
 
-    /**
-     * 根据id查询
-     */
+    @ApiOperation("根据id查询")
     @GetMapping("/findById.do")
     public Brand findById(Integer id) {
         return brandService.findById(id);
     }
 
-    /**
-     * 添加品牌
-     */
+    @ApiOperation("添加品牌")
     @PostMapping("/save.do")
     public CommonResponse<Brand> save(@RequestBody Brand brand) {
         return brandService.save(brand);
     }
 
-    /**
-     * 更新品牌
-     */
+    @ApiOperation("更新品牌")
     @PostMapping("/update.do")
     public CommonResponse<Brand> update(@RequestBody Brand brand) {
         return brandService.update(brand);
     }
-
-    /**
-     * 根据id删除品牌
-     */
+    
+    @ApiOperation("根据id删除品牌")
     @PostMapping("/delete.do")
     public CommonResponse<Brand> delete(Integer id) {
         return brandService.delete(id);
